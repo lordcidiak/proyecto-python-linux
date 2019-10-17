28b08567e9abede0c2706b2c3e911971b2de0bb9
juan pablo sanchez gaitan <50275221+lordcidiak@users.noreply.github.com>
2019-10-16 21:26:06 -0500

    actualización #3.1

--- agenda.py
+++ agenda.py
@@ -13,18 +13,26 @@ menu_choice = 0
 print_menu()
 while menu_choice != 7:
     menu_choice = input("Escribe un numero (1-7):")
+    
+    
     if menu_choice == 1:
         print ("Numeros de telefono:")
         for x in numbers.keys():
             print ("Nombre: ",x," \tNumero: ",numbers[x])
+            
+            
     elif menu_choice == 2:
         print ("Numeros de telefono:")
         for x in listablanca.keys():
             print ("Nombre: ",x," \tNumero: ",listablanca[x])
+            
+            
     elif menu_choice == 3:
         print( "Numeros de telefono:")
         for x in listanegra.keys():
             print ("Nombre: ",x," \tNumero: ",listanegra[x]  ) 
+            
+            
     elif menu_choice == 4:
         print ("ingresa el nombre, el numero y si va para la lista blanca o lista negra")
         nombre = raw_input("Nombre:")
@@ -35,21 +43,26 @@ while menu_choice != 7:
             listablanca[nombre] = numero
         elif lista == 2:
             listanegra[nombre] = numero
+            
+            
     elif menu_choice == 5:
         print ("eliminar el contacto")
-        name = raw_input("Nombre:")
+        nombre = raw_input("Nombre:")
         if numbers.has_key(nombre):
             del numbers[nombre]
             del listablanca[nombre]
             del listanegra[nombre]
         else:
             print( "Este ",nombre,"no se encuentra en la agenda ")
+            
+            
     elif menu_choice == 6:
         print ("buscar el numero de la persona")
-        name = raw_input("nombre:")
+        nombre = raw_input("nombre:")
         if numbers.has_key(nombre):
             print ("El numero es ",numbers[nombre])
         else:
             print("Este ",nombre,"no se encuentra en la agenda ")
-    elif menu_choice != 7:
-        print_menu()
+            
+            
+    
